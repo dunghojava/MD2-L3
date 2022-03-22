849640abc9bace7fc0f9b346d96a713b09741a10
dunghojava <dungho21397@gmail.com>
2022-03-22 11:33:37 +0700

    chuyen doi nhiet do

--- src/MaxTrongMang.java
+++ src/MaxTrongMang.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class MaxTrongMang {
+import java.util.Scanner;
+
+public class MaxTrongMang {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size sould not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter elenment" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " , at posotion " + index);
+    }
 }
